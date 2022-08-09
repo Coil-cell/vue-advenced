908f468139c543cecc606139e7bef2f3a43c84d6
coil <75682926+ilsung12@users.noreply.github.com>
2022-08-09 23:56:27 +0900

    📝  Apply: router-link

--- vue-news/src/App.vue
+++ vue-news/src/App.vue
@@ -1,14 +1,19 @@
 <template>
   <div id="app">
+    <tool-bar></tool-bar>
     <!-- router를 등록, main에서 연결, 자동으로 컴포넌트를 반영. -->
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import ToolBar from './components/ToolBar.vue';
+
 export default {
   name: 'App',
-  components: {},
+  components: {
+    ToolBar,
+  },
 };
 </script>
 
---
+++ vue-news/src/components/ToolBar.vue
@@ -0,0 +1,11 @@
+<template lang="">
+  <div>
+    <router-link to="/news">News</router-link>
+    <router-link to="/ask">Ask</router-link>
+    <router-link to="/jobs">Jobs</router-link>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style lang=""></style>
