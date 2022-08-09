06eb02946b2a4f48c32ec1051e5badff798aaeb2
coil <75682926+ilsung12@users.noreply.github.com>
2022-08-10 00:10:52 +0900

    💄  Add: Styling to TooBar Component

--- vue-news/src/components/ToolBar.vue
+++ vue-news/src/components/ToolBar.vue
@@ -1,11 +1,26 @@
-<template lang="">
-  <div>
-    <router-link to="/news">News</router-link>
-    <router-link to="/ask">Ask</router-link>
+<template>
+  <div class="header">
+    <router-link to="/news">News</router-link> |
+    <router-link to="/ask">Ask</router-link> |
     <router-link to="/jobs">Jobs</router-link>
   </div>
 </template>
 <script>
 export default {};
 </script>
-<style lang=""></style>
+<style scoped>
+.header {
+  color: white;
+  background-color: #42b883;
+  display: flex;
+  padding: 8px;
+}
+.header .router-link-exact-active {
+  color: #354952;
+  font-weight: bold;
+}
+
+.header a {
+  color: white;
+}
+</style>
