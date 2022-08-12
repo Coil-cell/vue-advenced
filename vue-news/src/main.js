ca98097fb1a24b0f8ed2c0b2c788ddc7373726b1
ilsungHana <a01041118225@gmail.com>
2022-08-12 17:19:34 +0900

    Add : Apply Vuex to VueMain

--- vue-news/src/main.js
+++ vue-news/src/main.js
@@ -1,6 +1,7 @@
-import Vue from 'vue';
-import App from './App.vue';
-import { router } from './routes/index.js';
+import Vue from "vue";
+import App from "./App.vue";
+import { router } from "./routes/index.js";
+import { store } from "./store/index.js";
 
 Vue.config.productionTip = false;
 
@@ -8,4 +9,5 @@ new Vue({
   // vue(App.vue) 에 넘겨줄 구조물들
   render: (h) => h(App),
   router, // 앞 뒤 의 값이 같을때 하나로 줄일수 있음 (=> router: router,)
-}).$mount('#app');
+  store,
+}).$mount("#app");
