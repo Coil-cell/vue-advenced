77dc7b22d7207a65b182aa3f144dcd971a558c19
coil <75682926+ilsung12@users.noreply.github.com>
2022-08-17 14:12:50 +0900

    📝  refectoring1 : NewView Components & styling

--- vue-news/src/views/NewsView.vue
+++ vue-news/src/views/NewsView.vue
@@ -1,14 +1,34 @@
 <template>
   <div>
-    <p v-for="item in this.$store.state.news" :key="item.id">
+    <ul class="news-list">
+      <li class="post" v-for="item in this.$store.state.news" :key="item.id">
+        <!-- 포인트 영역 -->
+        <div class="points">
+          {{ item.points }}
+        </div>
+        <!-- 기타 정보 영역 -->
+        <div>
+          <p class="news-title">
+            <a v-bind:href="item.url">{{ item.title }}</a>
+          </p>
+          <small class="link-text">
+            by
+            <router-link class="link-text" :to="`/user/${item.user}`">
+              {{ item.user }}</router-link
+            >
+          </small>
+        </div>
+      </li>
+    </ul>
+    <!-- <p v-for="item in this.$store.state.news" :key="item.id">
       <a v-bind:href="item.url">{{ item.title }}</a>
       <small>
         {{ item.time_ago }}
         by
-        <!-- <router-link :to="'/user/' + item.user"> {{ item.user }}</router-link> -->
+        
         <router-link :to="`/user/${item.user}`"> {{ item.user }}</router-link>
       </small>
-    </p>
+    </p> -->
   </div>
 </template>
 <script>
@@ -40,4 +60,29 @@ export default {
   },
 };
 </script>
-<style lang=""></style>
+<style scoped>
+.news-list {
+  margin: 0;
+  padding: 0;
+}
+.post {
+  list-style: none;
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid #eee;
+}
+.points {
+  width: 80px;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #42b883;
+}
+.news-title {
+  margin: 0;
+}
+.link-text {
+  color: #828282;
+}
+</style>
