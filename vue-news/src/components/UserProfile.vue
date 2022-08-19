a38321171c35262bcba085dfb6e90d0f560d4532
coil <75682926+ilsung12@users.noreply.github.com>
2022-08-20 00:17:23 +0900

    📝  refector2 : 1. Public Component to UserProfile

---
+++ vue-news/src/components/UserProfile.vue
@@ -0,0 +1,42 @@
+<template lang="">
+  <div class="user-container">
+    <div>
+      <i class="fas fa-user"></i>
+    </div>
+    <div class="user-description">
+      <router-link :to="`/user/${userInfo.user}`">
+        <div>
+          {{ userInfo.id }}
+        </div>
+      </router-link>
+      <div class="time">
+        {{ userInfo.created }}
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  computed: {
+    userInfo() {
+      return this.$store.state.users;
+    },
+  },
+};
+</script>
+<style scoped>
+.user-container {
+  display: flex;
+  align-items: center;
+  padding: 0.5rem;
+}
+.user-description {
+  padding-left: 8px;
+}
+.fa-user {
+  font-size: 2.5rem;
+}
+.time {
+  font-size: 0.75rem;
+}
+</style>
--- vue-news/src/views/UserView.vue
+++ vue-news/src/views/UserView.vue
@@ -1,17 +1,23 @@
 <template>
   <div>
-    <p>name: {{ userInfo.id }}</p>
+    <user-profile></user-profile>
+    <!-- <p>name: {{ userInfo.id }}</p>
     <p>karma: {{ userInfo.karma }}</p>
-    <p>created: {{ userInfo.created }}</p>
+    <p>created: {{ userInfo.created }}</p> -->
   </div>
 </template>
 <script>
+import UserProfile from '../components/UserProfile.vue';
+
 export default {
-  computed: {
-    userInfo() {
-      return this.$store.state.users;
-    },
+  components: {
+    UserProfile,
   },
+  // computed: {
+  //   userInfo() {
+  //     return this.$store.state.users;
+  //   },
+  // },
   created() {
     const userName = this.$route.params.id;
     this.$store.dispatch('FETCH_USER', userName);
