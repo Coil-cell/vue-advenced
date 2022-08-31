2c906dad0ff87f3d79dcdee30cb56a891c39af0c
coil <75682926+ilsung12@users.noreply.github.com>
2022-08-31 16:24:09 +0900

    💄 TheEnd : Edit Component for Design Styling

---
+++ design/common/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <app-header :title="appTitle"></app-header>
+    <app-content :items="items" @renew="renewItems"></app-content>
+  </div>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader.vue';
+import AppContent from './components/AppContent.vue';
+
+export default {
+  components: {
+    AppHeader,
+    AppContent,
+  },
+  data() {
+    return {
+      appTitle: 'Common Approach',
+      items: [10, 20, 30],
+    };
+  },
+  methods: {
+    renewItems() {
+      // this.items = [40, 50, 60];
+      var itemList = this.items;
+      // var itemLength = this.items.length();
+
+      for (var i in itemList) {
+        this.$set(this.items, i, (this.items[i] += 10));
+      }
+
+      console.log(this.items);
+    },
+  },
+};
+</script>
---
+++ design/slots/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <ul>
+      <item>아이템 1</item>
+      <item>아이템 2 <button>click me</button></item>
+      <item
+        ><div>아이템 3</div>
+        <img
+          src="./assets/endgame.png"
+          alt="end Game"
+          width="150px"
+          height="150px"
+      /></item>
+      <item>
+        <div style="color: blue; font-size: 20px;">
+          아이템 4
+        </div>
+      </item>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Item from './Item.vue';
+
+export default {
+  components: {
+    Item,
+  },
+};
+</script>
